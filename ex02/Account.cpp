890cab595e3c471c9fd393d8fbe10957136c7c1c
beeguy <beeguy74@gmail.com>
2021-08-27 09:09:50 +0300

    not working static funcs

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -9,11 +9,20 @@ Account::Account(int initial_deposit) : _amount(initial_deposit)
     this->_nbDeposits = 0;
     this->_nbWithdrawals = 0;
     Account::_displayTimestamp();
+    std::cout << " index:" << this->_accountIndex
+                << ";amount:" << this->_amount
+                << ";created" << std::endl;
     return ;
 }
 
 Account::~Account(void)
 {
+    Account::_nbAccounts--;
+    Account::_totalAmount -= this->_amount;
+    Account::_displayTimestamp();
+    std::cout << " index:" << this->_accountIndex
+                << ";amount:" << this->_amount
+                << ";closed" << std::endl;
     return ;
 }
 
@@ -26,7 +35,39 @@ void    Account::_displayTimestamp(void)
     stamp = time(NULL);
     local = std::localtime(&stamp);
     std::strftime(str, sizeof(str), "%Y%m%d_%H%M%S", local);
-    std::cout << '[' << str << ']' << std::endl;
+    std::cout << '[' << str << ']';
+    return ;
+}
+
+int Account::getNbAccounts(void)
+{
+    return Account::_nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+    return Account::_totalAmount;
+}
+
+// int Account::getNbDeposits(void)
+// {
+//     return Account::_nbDeposits;
+// }
+
+// int Account::getNbWithdrawals(void)
+// {
+//     return Account::_nbWithdrawals;
+// }
+
+void Account::displayAccountsInfos(void)
+{
+    Account::_displayTimestamp();
+    std::cout << " accounts:" << Account::getNbAccounts() 
+                << ";total:" << Account::getTotalAmount()
+                << ";deposits:" << Account::getNbDeposits()
+                << ";withdrawals:" << Account::getNbWithdrawals()
+                << std::endl;
+    return ;
 }
 
 int Account::_nbAccounts = 0;
@@ -37,5 +78,7 @@ int Account::_totalNbWithdrawals = 0;
 int main()
 {
     Account vasya(100);
+    Account petya(1100);
+    Account::displayAccountsInfos();
     return 0;
 }
