0c09558886e93ee219b1415512f54c03ec7bee58
Yaroslav Neverov <59573868+beeguy74@users.noreply.github.com>
2021-09-06 16:59:37 +0300

    not working mod01 ex04

--- mod_01/ex04/main.cpp
+++ mod_01/ex04/main.cpp
@@ -5,6 +5,7 @@
 bool    fileToStr(char *fileName, std::string &text){
     std::ifstream           inputFile;
     std::stringstream       strStream;
+
     inputFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     try {
         inputFile.open(fileName);
@@ -18,16 +19,44 @@ bool    fileToStr(char *fileName, std::string &text){
     return false;
 }
 
+bool    replace(std::string text, std::string s1, std::string s2)
+{
+    std::size_t     found;
+    std::size_t     pos;
+
+    found = 0;
+    while (true)
+    {
+        pos = found;
+        found = text.find(s1, pos);
+        if (found == std::string::npos)
+            return false;
+        text.erase(found, found + s1.size());
+        text.insert(found, s2);
+    }
+    return true;
+}
+
 int main(int argc, char** argv){
     bool        flag = false;
     std::string text;
 
-    if (argc < 2){
+    std::cerr << "HELLO" << std::endl;
+    if (argc != 4){
         std::cerr << "Wrong arguments" << std::endl;
     }
     flag = fileToStr(argv[1], text);
     if (flag)
+    {
+        std::cerr << "Wrong" << std::endl;
         return 1;
+    }
+    flag = replace(text, argv[2], argv[3]);
+    if (flag)
+    {
+        std::cerr << "Wro" << std::endl;
+        return 1;
+    }
     std::cout << text << std::endl;
     return 0;
 }
