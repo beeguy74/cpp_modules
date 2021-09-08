14911bbe7d8da7d1ab97062a71aabee7716b3bfc
Yaroslav Neverov <59573868+beeguy74@users.noreply.github.com>
2021-09-08 14:46:20 +0300

    mod01 ex01

--- mod_01/ex01/main.cpp
+++ mod_01/ex01/main.cpp
@@ -3,22 +3,24 @@
 
 Zombie  *zombieHorde(int N, std::string name);
 
+void    hordeAnnounce(int N, Zombie *arr){
+    for (int i = 0; i < N; i++){
+        arr->announce();
+    }
+    return ;
+}
 int main(void)
 {
     Zombie  *arr;
     int     N;
     N = 3;
     arr = zombieHorde(N, "Bill Murray");
-    for (int i = 0; i < N; i++){
-        arr->announce();
-    }
+    hordeAnnounce(N, arr);
     delete [] arr;
 
     N = 5;
     arr = zombieHorde(N, "Shon");
-    for (int i = 0; i < N; i++){
-        arr->announce();
-    }
+    hordeAnnounce(N, arr);
     delete [] arr;
     return 0;
 }
