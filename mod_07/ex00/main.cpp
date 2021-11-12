027f8e994d31e74dec988656c4a8a8804506e4b7
beeguy <beeguy74@gmail.com>
2021-11-12 10:08:02 +0300

    mod_07 ex00

--- mod_07/ex00/main.cpp
+++ mod_07/ex00/main.cpp
@@ -2,22 +2,26 @@
 #include "Fixed.hpp"
 
 int main(){
-    std::string hello = " Hello";
-    std::string world = " world";
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
-    std::cout << "MIN: " << min(hello, world) << std::endl;
-    std::cout << "MAX: " << max(hello, world) << std::endl;
-    swap(hello, world);
-    std::cout << "H: " <<hello << std::endl;
-    std::cout << "W: " << world << std::endl;
+    Fixed a1 = Fixed(10);
+    Fixed b1 = Fixed(2.1f);
 
-    Fixed a = Fixed(10);
-    Fixed b = Fixed(2.1f);
-
-    std::cout << "MIN: " << min(a, b) << std::endl;
-    std::cout << "MAX: " << max(a, b) << std::endl;
-    swap(a, b);
-    std::cout << "A: " << a << std::endl;
-    std::cout << "B:" <<  b << std::endl;
+    std::cout << "MIN: " << min(a1, b1) << std::endl;
+    std::cout << "MAX: " << max(a1, b1) << std::endl;
+    swap(a1, b1);
+    std::cout << "A: " << a1 << std::endl;
+    std::cout << "B:" <<  b1 << std::endl;
     return 0;
 }
